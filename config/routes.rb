0faaec5a47a3526149663fb2eca4b035e550c6a6
Forest Good <fgoodiu@gmail.com>
2013-08-22 18:03:46 -0400

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Emptysuit::Application.routes.draw do
+  root :to => 'static_pages#home'
+  
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Emptysuit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
