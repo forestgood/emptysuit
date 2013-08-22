b397b1178609c8c2362306d09d33517e49fbb1aa
Forest Good <fgoodiu@gmail.com>
2013-08-22 17:31:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emptysuit::Application.config.secret_token = '1a09729fe4c56a45ed4dfa802934c73a9a76481cf8d0ef0f9c54a7a45c03a4ad88cc0acef4b2d8e0ef221470688075e09225247dfa281c4f0b312b7f157477b3'
